5e96b79940ab6807c950150ca541cbf833353723
Aarumi Bots <214774540+itsAarumi@users.noreply.github.com>
2025-07-14 14:43:56 +0530

    Update start.py

--- AarumiMusic/plugins/bot/start.py
+++ AarumiMusic/plugins/bot/start.py
@@ -27,18 +27,18 @@ from AarumiMusic.utils.inline import first_page, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
-SHASHANK_VD = ["https://telegra.ph/file/89c5023101b65f21fb401.mp4",
-          "https://telegra.ph/file/bbc914cce6cce7f607641.mp4",
-          "https://telegra.ph/file/abc578ecc222d28a861ba.mp4",
-          "https://telegra.ph/file/065f40352707e9b5b7c15.mp4",
-          "https://telegra.ph/file/52ceaf02eae7eed6c9fff.mp4",
-          "https://telegra.ph/file/299108f6ac08f4e65e47a.mp4",
-          "https://telegra.ph/file/7a4e08bd04d628de71fc1.mp4",
-          "https://telegra.ph/file/0ad8b932fe5f7684f941c.mp4",
-          "https://telegra.ph/file/95ebe2065cfb1ac324a1c.mp4",
-          "https://telegra.ph/file/98cf22ccb987f9fedac5e.mp4",
-          "https://telegra.ph/file/f1b1754fc9d01998f24df.mp4",
-          "https://telegra.ph/file/421ee22ed492a7b8ce101.mp4"]
+AARUMI_PIC = ["https://files.catbox.moe/4rpo7o.jpg",
+          "https://files.catbox.moe/hgvcz6.jpg",
+          "https://files.catbox.moe/d5gvwa.jpg",
+          "https://files.catbox.moe/n3dh8g.jpg",
+          "https://files.catbox.moe/90ad4u.jpg",
+          "https://files.catbox.moe/vwe6s7.jpg",
+          "https://files.catbox.moe/yzd87s.jpg",
+          "https://files.catbox.moe/qsocxq.jpg",
+          "https://files.catbox.moe/3q5ej2.jpg",
+          "https://files.catbox.moe/weohk1.jpg",
+          "https://files.catbox.moe/k34956.jpg",
+          "https://files.catbox.moe/t9i9c7.jpg"]
 
 SHASHANK_PH = [
     "https://files.catbox.moe/jrupn9.jpg",
@@ -80,7 +80,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = first_page(_)
             await message.reply_video(
-                random.choice(SHASHANK_VD),
+                random.choice(AARUMI_PIC),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -134,7 +134,7 @@ async def start_pm(client, message: Message, _):
         served_users = len(await get_served_users())
         UP, CPU, RAM, DISK = await bot_sys_stats()
         await message.reply_video(
-            random.choice(SHASHANK_VD),
+            random.choice(AARUMI_PIC),
             caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM, served_users, served_chats),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -150,7 +150,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_video(
-        random.choice(SHASHANK_VD),
+        random.choice(AARUMI_PIC),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
