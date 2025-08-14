d6a2782024b683ce137714a5be202f62ba524433
˹ ᴧᴧʀᴜᴍɪ ʙᴏᴛ's ˼ <214774540+itsAarumi@users.noreply.github.com>
2025-08-15 01:00:08 +0530

    Update help.py

--- AarumiMusic/plugins/bot/help.py
+++ AarumiMusic/plugins/bot/help.py
@@ -10,23 +10,26 @@ from AarumiMusic.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
-AARUMI_PIC = ["https://files.catbox.moe/fh7vw7.jpg",
-          "https://files.catbox.moe/lckxh6.jpg",
-          "https://files.catbox.moe/smteo6.jpg",
-          "https://files.catbox.moe/7enu2i.jpg",
-          "https://files.catbox.moe/n6hkvd.jpg",
-          "https://files.catbox.moe/ej1p7t.jpg",
-          "https://files.catbox.moe/fh7vw7.jpg",
-          "https://files.catbox.moe/lckxh6.jpg",
-          "https://files.catbox.moe/smteo6.jpg",
-          "https://files.catbox.moe/7enu2i.jpg",
-          "https://files.catbox.moe/n6hkvd.jpg",
-          "https://files.catbox.moe/ej1p7t.jpg"]
+# Images list (cleaned spaces)
+AARUMI_PIC = [
+    "https://files.catbox.moe/fh7vw7.jpg",
+    "https://files.catbox.moe/lckxh6.jpg",
+    "https://files.catbox.moe/smteo6.jpg",
+    "https://files.catbox.moe/7enu2i.jpg",
+    "https://files.catbox.moe/n6hkvd.jpg",
+    "https://files.catbox.moe/ej1p7t.jpg",
+    "https://files.catbox.moe/fh7vw7.jpg",
+    "https://files.catbox.moe/lckxh6.jpg",
+    "https://files.catbox.moe/smteo6.jpg",
+    "https://files.catbox.moe/7enu2i.jpg",
+    "https://files.catbox.moe/n6hkvd.jpg",
+    "https://files.catbox.moe/ej1p7t.jpg"
+]
 
-
+# -------------------- Private Help Command --------------------
 @app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
-async def helper_private(client: app, update: Union[types.Message, types.CallbackQuery]):
+async def helper_private_command(client, update: Union[types.Message, types.CallbackQuery]):
     is_callback = isinstance(update, types.CallbackQuery)
     if is_callback:
         try:
@@ -54,94 +57,88 @@ async def helper_private(client: app, update: Union[types.Message, types.Callbac
             reply_markup=keyboard,
         )
 
+# -------------------- Fixed Callback Help --------------------
 @app.on_callback_query(filters.regex("settings_back_helper_fixed") & ~BANNED_USERS)
-async def helper_private(client: app, update: Union[types.Message, types.CallbackQuery]):
+async def helper_private_fixed(client, update: Union[types.Message, types.CallbackQuery]):
     is_callback = isinstance(update, types.CallbackQuery)
-
     if is_callback:
         try:
             await update.answer()
         except:
             pass
-
         chat_id = update.message.chat.id
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = first_page(_)
         await update.edit_message_text(
             _["help_1"].format(SUPPORT_CHAT),
-            reply_markup=keyboard)
-
+            reply_markup=keyboard
+        )
     else:
         try:
             await update.delete()
         except:
             pass
-
         language = await get_lang(update.chat.id)
         _ = get_string(language)
         keyboard = first_page(_)
-
         await update.reply_photo(
-            random.choice(AARUMI_PIC),            caption=_["help_1"].format(SUPPORT_CHAT),
-            reply_markup=keyboard)
+            random.choice(AARUMI_PIC),
+            caption=_["help_1"].format(SUPPORT_CHAT),
+            reply_markup=keyboard
+        )
 
-
+# -------------------- Group Help Command --------------------
 @app.on_message(filters.command(["help"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
     await message.reply_text(_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard))
 
-
+# -------------------- Help Callback --------------------
 @app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
 @languageCB
 async def helper_cb(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
     keyboard = help_back_markup(_)
-    if cb == "hb1":
-        await CallbackQuery.edit_message_text(helpers.HELP_1, reply_markup=keyboard)
-    elif cb == "hb2":
-        await CallbackQuery.edit_message_text(helpers.HELP_2, reply_markup=keyboard)
-    elif cb == "hb3":
-        await CallbackQuery.edit_message_text(helpers.HELP_3, reply_markup=keyboard)
-    elif cb == "hb4":
-        await CallbackQuery.edit_message_text(helpers.HELP_4, reply_markup=keyboard)
-    elif cb == "hb5":
-        await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
-    elif cb == "hb6":
-        await CallbackQuery.edit_message_text(helpers.HELP_6, reply_markup=keyboard)
-    elif cb == "hb7":
-        await CallbackQuery.edit_message_text(helpers.HELP_7, reply_markup=keyboard)
-    elif cb == "hb8":
-        await CallbackQuery.edit_message_text(helpers.HELP_8, reply_markup=keyboard)
-    elif cb == "hb9":
-        await CallbackQuery.edit_message_text(helpers.HELP_9, reply_markup=keyboard)
-    elif cb == "hb10":
-        await CallbackQuery.edit_message_text(helpers.HELP_10, reply_markup=keyboard)
-    elif cb == "hb11":
-        await CallbackQuery.edit_message_text(helpers.HELP_11, reply_markup=keyboard)
-    elif cb == "hb12":
-        await CallbackQuery.edit_message_text(helpers.HELP_12, reply_markup=keyboard)
-    elif cb == "hb13":
-        await CallbackQuery.edit_message_text(helpers.HELP_13, reply_markup=keyboard)
-    elif cb == "hb14":
-        await CallbackQuery.edit_message_text(helpers.HELP_14, reply_markup=keyboard)
-    elif cb == "hb15":
-        await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
-    elif cb == "hb16":
-        await CallbackQuery.edit_message_text(helpers.HELP_16, reply_markup=keyboard)
+    helpers_map = {
+        "hb1": helpers.HELP_1,
+        "hb2": helpers.HELP_2,
+        "hb3": helpers.HELP_3,
+        "hb4": helpers.HELP_4,
+        "hb5": helpers.HELP_5,
+        "hb6": helpers.HELP_6,
+        "hb7": helpers.HELP_7,
+        "hb8": helpers.HELP_8,
+        "hb9": helpers.HELP_9,
+        "hb10": helpers.HELP_10,
+        "hb11": helpers.HELP_11,
+        "hb12": helpers.HELP_12,
+        "hb13": helpers.HELP_13,
+        "hb14": helpers.HELP_14,
+        "hb15": helpers.HELP_15,
+        "hb16": helpers.HELP_16,
+    }
+    if cb in helpers_map:
+        await CallbackQuery.edit_message_text(helpers_map[cb], reply_markup=keyboard)
 
-Shiv_Text = ("ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴄᴧᴛᴇɢᴏʀʏ ғᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴧɴɴᴧ ɢᴇᴛ ʜᴇʟᴩ.\nᴧsᴋ ʏᴏᴜʀ ᴅᴏᴜʙᴛs ᴧᴛ <a href={0}>sᴜᴘᴘᴏʀᴛ ᴄʜᴧᴛ</a>\n\nᴧʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴧɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ: <code>/</code>")
+# -------------------- Shiv Menu --------------------
+Shiv_Text = (
+    "ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴄᴧᴛᴇɢᴏʀʏ ғᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴧɴɴᴧ ɢᴇᴛ ʜᴇʟᴩ.\n"
+    "ᴧsᴋ ʏᴏᴜʀ ᴅᴏᴜʙᴛs ᴧᴛ <a href={0}>sᴜᴘᴘᴏʀᴛ ᴄʜᴧᴛ</a>\n\n"
+    "ᴧʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴧɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ: <code>/</code>"
+)
 
 @app.on_callback_query(filters.regex("shivXaarumi") & ~BANNED_USERS)
 @languageCB
 async def first_pagexx(client, CallbackQuery, _):
     menu_next = second_page(_)
     try:
-        await CallbackQuery.message.edit_text(Shiv_Text, reply_markup=menu_next)
-        return
+        await CallbackQuery.message.edit_text(
+            Shiv_Text.format(SUPPORT_CHAT),
+            reply_markup=menu_next
+        )
     except:
         return
 
@@ -150,11 +147,13 @@ async def first_pagexx(client, CallbackQuery, _):
 async def first_pagee(client, CallbackQuery, _):
     menu_next = second_page(_)
     try:
-        await CallbackQuery.message.edit_text(Shiv_Text, reply_markup=menu_next)
-        return
+        await CallbackQuery.message.edit_text(
+            Shiv_Text.format(SUPPORT_CHAT),
+            reply_markup=menu_next
+        )
     except:
         return
 
-
+# -------------------- End --------------------
 # Do not try to change whole code, just add or remove what you want.
 # Credited To Aarumi
