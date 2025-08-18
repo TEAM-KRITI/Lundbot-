5f4cf245f099d54eb5c614798ade0677a9b35233
TEAM KRITI BOTS <rejemtahir@gmail.com>
2025-08-18 13:49:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TEAM-KRITI/Lundbot",
+    "https://github.com/TEAM-KRITI/Lundbot-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
