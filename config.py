8392282234a2bbf948cb015ab22cf6ebca709e8f
⎯᪵⎯꯭̽🚬᪵᪳ ⃪꯭𝐁ʌ‌꯭ᴅ꯭𝐍꯭ᴀ‌꯭ᴍ꯭ ꯭🖤᪵᪳᪳𝆺꯭𝅥⎯꯭̽⟶᯦꯭ <rejemtahir@gmail.com>
2025-09-18 20:19:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOGGER_ID = int(getenv("LOGGER_ID", ""))
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", "30DxNexGenBots39e64f")
+API_KEY = getenv("API_KEY", "NxGBNexGenBots0bbc08")
 
 OWNER_ID = int(getenv("OWNER_ID", "8486348579"))
 
